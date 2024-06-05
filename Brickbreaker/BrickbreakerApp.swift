d5a864526f985cbadc85cc8800dd1e156afd3164
Jay D <jdavenz8@gmail.com>
2024-06-05 15:13:26 +1200

    Initial Commit

---
+++ Brickbreaker/BrickbreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrickbreakerApp.swift
+//  Brickbreaker
+//
+//  Created by Jigar on 05/06/24.
+//
+
+import SwiftUI
+
+@main
+struct BrickbreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
